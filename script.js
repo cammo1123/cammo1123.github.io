4463106ba2d75c77f8d7435321937a8ba27e321e
Cameron Youell <cameronyouell@gmail.com>
2021-07-26 21:42:32 +1000

    Removed Seperate Navbar Until Better Method without loading time is discovered :)

--- script.js
+++ script.js
@@ -18,3 +18,93 @@ function getCookie(name) {
     }
     return null;
 }
+
+let prevID = ""
+$("#collapse").click(() => {
+    collapsed = getCookie("collapsed") == "true"
+    setCollapsed(!collapsed)
+})
+
+$("#adjust").click(() => {
+    dark = getCookie("dark") == "true"
+    setDark(!dark)
+})
+
+function setDark(dark) {
+    if (!dark) {
+        document.documentElement.style.setProperty('--background-color', "#fff");
+        document.documentElement.style.setProperty('--text-color', "#000");
+    } else {
+        document.documentElement.style.setProperty('--background-color', "#181a1b");
+        document.documentElement.style.setProperty('--text-color', "#e8e6e3");
+    }
+
+    setCookie("dark", `${dark}`, 30);
+}
+
+function setCollapsed(collapsed) {
+    if (!collapsed) {
+        document.documentElement.style.setProperty('--toolbar-width', "300px");
+        document.documentElement.style.setProperty('--toolbar-text', "unset");
+        document.documentElement.style.setProperty('--nav-color', "#23a6d5");
+
+        $("#collapse").html("<i class='bx bxs-chevron-left'></i>")
+    } else {
+        document.documentElement.style.setProperty('--toolbar-width', "50px");
+        document.documentElement.style.setProperty('--toolbar-text', "none");
+        document.documentElement.style.setProperty('--nav-color', "transparent");
+
+        $("#collapse").html("<i class='bx bxs-chevron-right'></i>")
+
+    }
+
+    setCookie("collapsed", `${collapsed}`, 30);
+}
+
+function setPage(page) {
+    let pos = page == "" ? $(`#index`).offset() : $(`#${page}`).offset();
+
+    if (prevID != "") {
+        $(`#${prevID}`).removeClass("active")
+    }
+
+    $(`#${page}`).addClass("active")
+
+    let my_pos = $("#active_cover")
+
+    let my_pos_height = my_pos.height() + 24
+
+    $("#top_fill").css("bottom", ((window.innerHeight - (pos.top - my_pos.height() + 24)) - 14) + scrollY)
+    $("#bottom_fill").css("top", ((my_pos_height + (pos.top - my_pos.height() + 24)) + 14) - scrollY)
+    $("#active_cover").css("top", ((pos.top - my_pos.height() + 24) + 14) - scrollY);
+
+    prevID = this.id;
+}
+
+let path = window.location.pathname;
+let page = path.split("/").pop().split(".html")[0];
+let links = document.getElementsByClassName("item_container");
+
+setPage(page);
+
+collapsed = getCookie("collapsed") != "false";
+setCollapsed(collapsed);
+
+dark = getCookie("dark") == "true";
+setDark(dark);
+
+
+function addAnimations() {
+    $(".cover").children().addClass("transition")
+}
+
+window.addEventListener('resize', () => {
+    setPage(page);
+});
+
+
+$(".item_container").click(function(e) {
+    setPage(this.id);
+
+    window.location.href = (`${this.id}.html`)
+});
