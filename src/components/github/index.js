f618e208f2411c3ce32d9b20278d30096f45ed94
Cameron Youell <cameronyouell@gmail.com>
2021-07-29 15:04:33 +1000

    Added automatic fetching of github repos

--- src/components/github/index.js
+++ src/components/github/index.js
@@ -1,15 +1,65 @@
-import './style.css'
+import "./style.css";
+import React, { Component } from "react";
 
-export function githubCard(name, des, link) {
-	return (
-		<a alt={name} href={link} target="_blank" rel="noreferrer" className="githubCard">
-			<div>
-				<img alt="github avatar" src="https://avatars.githubusercontent.com/u/36214028" />
+export class GitHubSearch extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	getUserRepo(username) {
+		return fetch(`https://api.github.com/users/${username}/repos`)
+			.then((response) => response.json())
+			.then((response) => {
+				return response;
+			});
+	}
+
+	async componentDidMount() {
+        let repos = await this.getUserRepo("cammo1123");
+		this.setState({ repos: repos });
+    }
+
+	async handleSubmit(e) {
+		e.preventDefault();
+		let repos = await this.getUserRepo(e.target[0].value);
+		this.setState({ repos: repos });
+	}
+
+	render() {
+		let repos = [];
+
+		if (this.state.repos) {
+			for (let i = 0; i < this.state.repos.length; i++) {
+				let repo = this.state.repos[i];
+				if (repo.private) break;
+				repos.push(
+					<a key={i} alt={repo.full_name} href={repo.html_url} target="_blank" rel="noreferrer" className="githubCard">
+						<div>
+							<h1>{repo.full_name}</h1>
+							<p>{repo.description}</p>
+							<span>
+								<i className="bx bx-git-repo-forked"></i>
+								{repo.forks_count}
+								<i className="bx bx-star"></i>
+								{repo.stargazers_count}
+							</span>
+						</div>
+					</a>
+				);
+			}
+		}
+
+		return (
+			<div className="githubSearch">
+				<header>
+					<h1>Repositories for</h1>
+					<form onSubmit={(e) => this.handleSubmit(e)}>
+						<input type="text" placeholder="username" defaultValue="cammo1123"/>
+					</form>
+				</header>
+				<div className="githubGrid">{repos}</div>
 			</div>
-			<div>
-				<h1>cammo1123/{name}</h1>
-				<p>{des}</p>
-			</div>
-		</a>
-	);
+		);
+	}
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,16 +1,11 @@
-import { setPage } from "../../components/functions"
-import { githubCard } from "../../components/github"
+import { setPage } from "../../components/functions";
+import { GitHubSearch } from "../../components/github";
 
 export function Projects() {
 	setPage("projects");
 	return (
 		<div className="content">
-			<h1> Current Projects </h1>
-			<div className="githubGrid">
-				{githubCard("PlexPlayer", "A simple player for non Plex premium users", "https://github.com/cammo1123/Plex-Player")}
-				{githubCard("Sentral_Companion", "ARCHIVED - A Companion app for https://web2.schoolName.schools.nsw.edu.au/portal ", "https://github.com/cammo1123/sentral_companion")}
-				{githubCard("cammo1123.github.io", "This Website :^)", "https://github.com/cammo1123/cammo1123.github.io")}
-			</div>
+			<GitHubSearch />
 		</div>
 	);
 }
