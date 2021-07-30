ea62b7d8b1238758d5b3baea0415bfb81fd9f3c6
Cameron Youell <cameronyouell@gmail.com>
2021-07-30 11:27:08 +1000

    Removed NavCoverImport and added switching for blog and portfolio

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./index.css";
 
 import { setCollapsed, setDark, setPage } from "./components/functions";
-import { NavBar, NavCover, NavItem } from "./components/navbar";
+import { NavBar, NavItem } from "./components/navbar";
 
 let arrowDir = setCollapsed(null, true);
 export let LINK = Link;
@@ -15,6 +15,8 @@ setDark(null, true);
 const Socials = lazy(() => import("./pages/socials"));
 const Home = lazy(() => import("./pages/home"));
 const Projects = lazy(() => import("./pages/projects"));
+const Blog = lazy(() => import("./pages/blog"));
+const Portfolio = lazy(() => import("./pages/portfolio"));
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -24,11 +26,19 @@ ReactDOM.render(
 				<NavItem id="socials" iconName="message" />
 				<NavItem id="projects" iconName="grid-alt" />
 			</NavBar>
-			<Suspense fallback={<div className="center fullscreen content">Loading...</div>}>
+			<Suspense
+				fallback={
+					<div className="center fullscreen">
+						<div className="content">Loading...</div>
+					</div>
+				}
+			>
 				<Switch>
 					<Route exact path="/" component={Home} />
 					<Route path="/socials" component={Socials} />
 					<Route path="/projects" component={Projects} />
+					<Route path="/blog" component={Blog} />
+					<Route path="/portfolio" component={Portfolio} />
 				</Switch>
 			</Suspense>
 		</Router>
