dd70c01affd9c7cb20a4fe0a97bb500089a9079f
Cameron Youell <cameronyouell@gmail.com>
2024-09-04 23:54:01 +1000

    Remove silly things

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,23 @@
 import { render } from "solid-js/web";
-import { Component, lazy, Suspense } from "solid-js";
-import { Routes, Route, Router } from "@solidjs/router";
-import { ContentPane, PageContent, PageSection } from "./components/page";
-import { NavBar, NavItem } from "./components/navbar";
+import { Component, lazy } from "solid-js";
+import { Route, Router } from "@solidjs/router";
+import { Meta, MetaProvider } from "@solidjs/meta";
 
 import "./index.scss";
 
 const Home = lazy(() => import("./pages/home"));
-const Socials = lazy(() => import("./pages/socials"));
-const Projects = lazy(() => import("./pages/projects"));
 
 const App: Component = () => (
-	<Router>
-		<NavBar>
-			<NavItem id="home" iconName="bx-home" path="/" />
-			<NavItem id="socials" iconName="bx-message" />
-			<NavItem id="projects" iconName="bx-git-repo-forked" />
-		</NavBar>
-		<ContentPane>
-			<Suspense
-				fallback={
-					<PageContent name="portfolio">
-						<PageSection class="fullpage center">
-							<div class="content">Loading...</div>
-						</PageSection>
-					</PageContent>
-				}>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/socials" element={<Socials />} />
-					<Route path="/projects" element={<Projects />} />
-				</Routes>
-			</Suspense>
-		</ContentPane>
-	</Router>
+	<>
+		<MetaProvider>
+			<Meta name="description" content="Cameron Youell's Personal website" />
+			<Meta property="og:title" content="Cameron Youell" />
+			<Meta property="og:description" content="Cameron Youell's Personal website" />
+		</MetaProvider>
+		<Router>
+			<Route path="/" component={Home} />
+		</Router>
+	</>
 );
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
