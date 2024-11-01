e97aaf44b8140f95f65433bda58a5311ac02d292
cammo1123 <cameronyouell@gmail.com>
2024-11-01 10:52:04 +0000

    Deploying to gh-pages from @ cammo1123/cammo1123.github.io@4f7bed6a63be0e7277cc00ba69424debdcc5c9f0 🚀

--- _build/assets/index-BWauLM0T.js
+++ _build/assets/index-BWauLM0T.js
@@ -6,14 +6,13 @@ import{q as n,x as t,j as d,F as c,c as p,d as C,G as S,z as o,H as T}from"./ind
             <br>
             This system should also be used alongside the principle of least privilege.  The principle of least privilege is where users/processes get the minimum amount of privilege to complete the task. So, for example, an employee who needs to modify users' emails would not get the ability to change all the user's information. Another example would be if an employee needs to read files/folders from a shared network drive (server), they would not be able to write/modify files/folders as that is not needed to read from the server.
             `},{title:"Twitch Cybersecurity Incident Response Plan",date:new Date(Date.parse("27 Apr 2022")),author:"Cameron Youell",content:`
-A productive and intelligent cyber security incident response plan could have facilitated a better response when related to the identification, response and mitigation of the impacts of the breach. Therefore, I will briefly go over some proactive steps that could have aided in the prevention/mitigation of the outcome.
-
-<h4>Using bare minimal privileges</h4>
-Bare minimal access is a method of proactive mitigation. It is where an administrator looks at the sort of information/services a user will need and gives them the bare minimum amount of privileges. This means that if the user gets hacked, the hacker is extremely limited in the amount of information/damage they can cause
-<h4>Monitoring access to high-risk data</h4>
-Monitoring high-risk data access would allow administrators to quickly tell if something had assessed data that was unexpected and set up methods for incidents to be automatically detected and then alert system administrators.
-<h4>Have an immediate template response for the media</h4>
-In the case of twitch, it would have been wise to have a template response for hacks and incidents like these. It would allow them to instantly get on top of speculation and prevent misinformed assumptions from impacting them as a business by creating internet-fueled mass panic.
-<h4>Inform users on how to minimise the impact</h4>
-Twitch should have also sent out emails to all its users, informing them to, as a precaution, change their passwords, and be on the lookout for unauthorised access to personal information. The communication between Twitch and its users would further reduce the impact on users and their businesses.
+            A productive and intelligent cyber security incident response plan could have facilitated a better response when related to the identification, response and mitigation of the impacts of the breach. Therefore, I will briefly go over some proactive steps that could have aided in the prevention/mitigation of the outcome.
+            <h4>Using bare minimal privileges</h4>
+            Bare minimal access is a method of proactive mitigation. It is where an administrator looks at the sort of information/services a user will need and gives them the bare minimum amount of privileges. This means that if the user gets hacked, the hacker is extremely limited in the amount of information/damage they can cause
+            <h4>Monitoring access to high-risk data</h4>
+            Monitoring high-risk data access would allow administrators to quickly tell if something had assessed data that was unexpected and set up methods for incidents to be automatically detected and then alert system administrators.
+            <h4>Have an immediate template response for the media</h4>
+            In the case of twitch, it would have been wise to have a template response for hacks and incidents like these. It would allow them to instantly get on top of speculation and prevent misinformed assumptions from impacting them as a business by creating internet-fueled mass panic.
+            <h4>Inform users on how to minimise the impact</h4>
+            Twitch should have also sent out emails to all its users, informing them to, as a precaution, change their passwords, and be on the lookout for unauthorised access to personal information. The communication between Twitch and its users would further reduce the impact on users and their businesses.
             `}];return[d(A,{children:"ACOM1001"}),(()=>{var a=n(O),e=a.firstChild,s=e.nextSibling;return t(s,d(H,{posts:l})),a})()]};export{B as default};
